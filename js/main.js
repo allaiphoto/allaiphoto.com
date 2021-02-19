d60f97adcd0ed9a6eca003a5e46cf94e8e37c28b
karmelalla <karmelalla@gmail.com>
2021-02-18 22:09:32 -0800

    major update

--- js/main.js
+++ js/main.js
@@ -9,7 +9,13 @@ jQuery(document).ready(function($) {
 	"use strict";
 
 	FlexMasonry.init('.grid', {
-    responsive: true
+    responsive: true,
+    breakpointCols: {
+      'min-width: 1200px': 5,
+      'min-width: 992px': 4,
+      'min-width: 768px': 3,
+      'min-width: 576px': 2
+    }
 	});
 
 	var gallery = function() {
