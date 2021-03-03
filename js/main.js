1f237cdfa70275319ba03f25069c41a49bfaa3a3
karmelalla <karmelalla@gmail.com>
2021-03-03 14:44:21 -0800

    all albums except Romantic

--- js/main.js
+++ js/main.js
@@ -12,9 +12,7 @@ jQuery(document).ready(function($) {
     responsive: true,
     breakpointCols: {
       'min-width: 1200px': 5,
-      'min-width: 992px': 4,
-      'min-width: 768px': 3,
-      'min-width: 576px': 2
+      'min-width: 600px': 3
     }
 	});
 
