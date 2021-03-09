f72aa33255e106a76b8b0e1a2280f4a706351bcf
Kirill Isakov <isakov90@gmail.com>
2021-03-08 16:13:07 -0800

    convert tabs

--- js/main.js
+++ js/main.js
@@ -1,48 +1,48 @@
  AOS.init({
- 	duration: 500,
- 	easing: 'slide',
- 	once: true
+   duration: 500,
+   easing: 'slide',
+   once: true
  });
 
 jQuery(document).ready(function($) {
 
-	"use strict";
+  "use strict";
 
   sendMessage('.send-message', true);
 
-	FlexMasonry.init('.grid', {
+  FlexMasonry.init('.grid', {
     responsive: true,
     breakpointCols: {
       'min-width: 1200px': 5,
       'min-width: 600px': 3
     }
-	});
+  });
 
-	var gallery = function() {
-		$('[data-fancybox="gallery"]').fancybox({
-			loop: true,
-			protect: true,
-			buttons: [
-				"share",
-				"slideShow",
-				"fullScreen",
-				"thumbs",
-				"close"
-			]
-		});
-	}
-	gallery();
+  var gallery = function() {
+    $('[data-fancybox="gallery"]').fancybox({
+      loop: true,
+      protect: true,
+      buttons: [
+        "share",
+        "slideShow",
+        "fullScreen",
+        "thumbs",
+        "close"
+      ]
+    });
+  }
+  gallery();
 
-	var siteMenuClone = function() {
+  var siteMenuClone = function() {
 
-		$('.js-clone-nav').each(function() {
-			var $this = $(this);
-			$this.clone().attr('class', 'site-nav-wrap').appendTo('.site-mobile-menu-body');
-		});
+    $('.js-clone-nav').each(function() {
+      var $this = $(this);
+      $this.clone().attr('class', 'site-nav-wrap').appendTo('.site-mobile-menu-body');
+    });
 
-		setTimeout(function() {
+    setTimeout(function() {
 
-			var counter = 0;
+      var counter = 0;
       $('.site-mobile-menu .has-children').each(function(){
         var $this = $(this);
 
@@ -64,7 +64,7 @@ jQuery(document).ready(function($) {
 
     }, 1000);
 
-		$('body').on('click', '.arrow-collapse', function(e) {
+    $('body').on('click', '.arrow-collapse', function(e) {
       var $this = $(this);
       if ( $this.closest('li').find('.collapse').hasClass('show') ) {
         $this.removeClass('active');
@@ -75,114 +75,114 @@ jQuery(document).ready(function($) {
 
     });
 
-		$(window).resize(function() {
-			var $this = $(this),
-				w = $this.width();
+    $(window).resize(function() {
+      var $this = $(this),
+        w = $this.width();
 
-			if ( w > 768 ) {
-				if ( $('body').hasClass('offcanvas-menu') ) {
-					$('body').removeClass('offcanvas-menu');
-				}
-			}
-		})
+      if ( w > 768 ) {
+        if ( $('body').hasClass('offcanvas-menu') ) {
+          $('body').removeClass('offcanvas-menu');
+        }
+      }
+    })
 
-		$('body').on('click', '.js-menu-toggle', function(e) {
-			var $this = $(this);
-			e.preventDefault();
+    $('body').on('click', '.js-menu-toggle', function(e) {
+      var $this = $(this);
+      e.preventDefault();
 
-			if ( $('body').hasClass('offcanvas-menu') ) {
-				$('body').removeClass('offcanvas-menu');
-				$this.removeClass('active');
-			} else {
-				$('body').addClass('offcanvas-menu');
-				$this.addClass('active');
-			}
-		})
+      if ( $('body').hasClass('offcanvas-menu') ) {
+        $('body').removeClass('offcanvas-menu');
+        $this.removeClass('active');
+      } else {
+        $('body').addClass('offcanvas-menu');
+        $this.addClass('active');
+      }
+    })
 
-		// click outisde offcanvas
-		$(document).mouseup(function(e) {
-	    var container = $(".site-mobile-menu");
-	    if (!container.is(e.target) && container.has(e.target).length === 0) {
-	      if ( $('body').hasClass('offcanvas-menu') ) {
-					$('body').removeClass('offcanvas-menu');
-				}
-	    }
-		});
-	};
-	siteMenuClone();
-
-
-	var siteCarousel = function () {
-		if ( $('.nonloop-block-13').length > 0 ) {
-			$('.nonloop-block-13').owlCarousel({
-		    center: false,
-		    items: 1,
-		    loop: false,
-				stagePadding: 0,
-		    margin: 20,
-		    nav: true,
-				navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
-		    responsive:{
-	        600:{
-	        	margin: 20,
-	          items: 2
-	        },
-	        1000:{
-	        	margin: 20,
-	        	stagePadding: 0,
-	          items: 2
-	        },
-	        1200:{
-	        	margin: 20,
-	        	stagePadding: 0,
-	          items: 3
-	        }
-		    }
-			});
-		}
-
-		$('.slide-one-item').owlCarousel({
-	    center: false,
-	    items: 1,
-	    loop: true,
-			stagePadding: 0,
-	    margin: 0,
-	    autoplay: true,
-	    pauseOnHover: false,
-	    nav: true,
-	    navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
-	  });
-	};
-	siteCarousel();
+    // click outisde offcanvas
+    $(document).mouseup(function(e) {
+      var container = $(".site-mobile-menu");
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+        if ( $('body').hasClass('offcanvas-menu') ) {
+          $('body').removeClass('offcanvas-menu');
+        }
+      }
+    });
+  };
+  siteMenuClone();
 
 
-	var swiperSetting = function() {
-		var mySwiper = new Swiper ('.swiper-container', {
-	    // Optional parameters
-	    // direction: 'horizontal',
-	    // loop: true,
+  var siteCarousel = function () {
+    if ( $('.nonloop-block-13').length > 0 ) {
+      $('.nonloop-block-13').owlCarousel({
+        center: false,
+        items: 1,
+        loop: false,
+        stagePadding: 0,
+        margin: 20,
+        nav: true,
+        navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
+        responsive:{
+          600:{
+            margin: 20,
+            items: 2
+          },
+          1000:{
+            margin: 20,
+            stagePadding: 0,
+            items: 2
+          },
+          1200:{
+            margin: 20,
+            stagePadding: 0,
+            items: 3
+          }
+        }
+      });
+    }
+
+    $('.slide-one-item').owlCarousel({
+      center: false,
+      items: 1,
+      loop: true,
+      stagePadding: 0,
+      margin: 0,
+      autoplay: true,
+      pauseOnHover: false,
+      nav: true,
+      navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
+    });
+  };
+  siteCarousel();
+
+
+  var swiperSetting = function() {
+    var mySwiper = new Swiper ('.swiper-container', {
+      // Optional parameters
+      // direction: 'horizontal',
+      // loop: true,
       autoplay: {
         delay: 2500,
         disableOnInteraction: false,
       },
-	    // If we need pagination
-	    pagination: {
-	      el: '.swiper-pagination',
-	    },
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
 
-	    // Navigation arrows
-	    navigation: {
-	      nextEl: '.swiper-button-next',
-	      prevEl: '.swiper-button-prev',
-	    },
-	    mousewheel: {
-		  	invert: false,
-		  	forceToAxis: true,
-		  	releaseOnEdges: true,
-		  },
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      mousewheel: {
+        invert: false,
+        forceToAxis: true,
+        releaseOnEdges: true,
+      },
 
-		  // direction: 'vertical',
-		  freeMode: true,
+      // direction: 'vertical',
+      freeMode: true,
       // slidesPerView: 'auto',
       spaceBetween: 300,
       mousewheel: true,
@@ -191,29 +191,29 @@ jQuery(document).ready(function($) {
         clickable: true,
       },
 
-	    // And if we need scrollbar
-	    // scrollbar: {
-	    //   el: '.swiper-scrollbar',
-	    // },
+      // And if we need scrollbar
+      // scrollbar: {
+      //   el: '.swiper-scrollbar',
+      // },
 
-	    slidesPerView: 3,
-			breakpoints: {
-				668: {
-					slidesPerView: 1
-				},
-				1024: {
-					slidesPerView: 2
-				}
-			},
-			// paginationClickable: false,
-			spaceBetween: 20,
-			// freeMode: true,
-			// grabCursor: true,
-			// mousewheelControl: true
+      slidesPerView: 3,
+      breakpoints: {
+        668: {
+          slidesPerView: 1
+        },
+        1024: {
+          slidesPerView: 2
+        }
+      },
+      // paginationClickable: false,
+      spaceBetween: 20,
+      // freeMode: true,
+      // grabCursor: true,
+      // mousewheelControl: true
 
-	  })
-	}
-	swiperSetting();
+    })
+  }
+  swiperSetting();
 
 });
 
