f9a349026a806b03247444d12480b1e31e4a8123
Alla Isakova <karmelalla@gmail.com>
2022-11-06 21:15:13 -0800

    fix js

--- js/main.js
+++ js/main.js
@@ -150,51 +150,6 @@ jQuery(document).ready(function($) {
   };
   siteMenuClone();
 
-
-  var siteCarousel = function () {
-    if ( $('.nonloop-block-13').length > 0 ) {
-      $('.nonloop-block-13').owlCarousel({
-        center: false,
-        items: 1,
-        loop: false,
-        stagePadding: 0,
-        margin: 20,
-        nav: true,
-        navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
-        responsive:{
-          600:{
-            margin: 20,
-            items: 2
-          },
-          1000:{
-            margin: 20,
-            stagePadding: 0,
-            items: 2
-          },
-          1200:{
-            margin: 20,
-            stagePadding: 0,
-            items: 3
-          }
-        }
-      });
-    }
-
-    $('.slide-one-item').owlCarousel({
-      center: false,
-      items: 1,
-      loop: true,
-      stagePadding: 0,
-      margin: 0,
-      autoplay: true,
-      pauseOnHover: false,
-      nav: true,
-      navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
-    });
-  };
-  siteCarousel();
-
-
   var swiperSetting = function() {
     var mySwiper = new Swiper ('.swiper-container', {
       // Optional parameters
